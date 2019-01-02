5f4589d6e619ef03fb6a39fee8be644b6dcea7ee
Daniel Cardozo C <daniel.cardozo0621@gmail.com>
2019-01-02 18:53:23 -0500

    Animation, Chart, Coins and Notes components added

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,54 @@ import React, { Component } from 'react';
 
 // import Home from './Home/Home';
 //import Todo from './Todo/Todo';
-import Timer from './Pomodoro/Timer';
+// import Timer from './Pomodoro/Timer';
+// import Coins from './Coins/Coins';
+// import Notes from './Notes/Notes';
+// import Chart from './Chart/Chart';
+import Animation from './Animation/Animation';
 
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
 import Footer from '../shared/components/layout/Footer';
 
+/* For Notes Component */
+// import { notes1, notes2 } from './Notes/data';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            /* For Notes Component */
+            // notes: notes1
+            chartType: 'line'
+        };
+
+        this.columns = [
+            ['BTC', 3000, 6000, 10000, 15000, 13000, 11000],
+            ['ETH', 2000, 3000, 5000, 4000, 3000, 940],
+            ['XRP', 100, 200, 300, 500, 400, 300],
+        ];
+    }
+
+    componentDidMount() {
+        /* For Notes Component */
+        // setTimeout(() => {
+        //     this.setState({ notes: [...this.state.notes, ...notes2] });
+        // }, 10000);
+    }
+
+    /* For Chart Component */
+    // setBarChart = () => {
+    //     this.setState({ chartType: 'bar' });
+    // };
+
+    /* For Chart Component */
+    // setLineChart = () => {
+    //     this.setState({ chartType: 'line' });
+    // };
+
     render() {
         return (
             <div className="App">
@@ -18,7 +57,20 @@ class App extends Component {
                 <Content>
                     {/*<Home />*/}
                     {/*<Todo />*/}
-                    <Timer />
+                    {/*<Timer />*/}
+                    {/*<Coins />*/}
+                    {/*<Notes notes={this.state.notes} />*/}
+
+                    {/*<div>
+                        <Chart columns={this.columns} chartType={this.state.chartType} />
+                        <p>
+                            Chart Type
+                            <button onClick={this.setBarChart}>Bar</button>
+                            <button onClick={this.setLineChart}>Line</button>
+                        </p>
+                    </div>*/}
+
+                    <Animation />
                 </Content>
                 <Footer />
             </div>
