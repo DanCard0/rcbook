921ae3e55b5178214dc84e234b1e2475d8b04c28
Daniel Cardozo C <daniel.cardozo0621@gmail.com>
2019-01-21 16:51:35 -0500

    Router functionality added in chap3

--- chap3/src/components/App.jsx
+++ chap3/src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Popup from 'react-popup';
 import Helmet from 'react-helmet';
+import { element } from 'prop-types';
 
 // import Calculator from "./Calculator/Calculator";
-import Person from './Person/Person';
+// import Person from './Person/Person';
 
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
@@ -12,12 +13,12 @@ import Footer from '../shared/components/layout/Footer';
 import './App.css';
 import './Popup.css';
 
-const App = () => (
+const App = props => (
   <div className="App">
     <Helmet
-      title="Chapter 3"
+      title="Chapters 3 and 4"
       meta={[
-        { name: 'title', content: 'Chapter 3' },
+        { name: 'title', content: 'Chapters 3 and 4' },
         { name: 'description', content: 'This recipe talks about React Cookbook' }
       ]}
     />
@@ -25,12 +26,17 @@ const App = () => (
     <Header title="Welcome to new App" />
     <Content>
       {/* <Calculator /> */}
-      <Person />
+      {/*<Person />*/}
+      {props.children}
     </Content>
     <Footer />
 
     <Popup />
   </div>
 );
+
+App.propTypes = {
+  children: element
+};
 
 export default App;
