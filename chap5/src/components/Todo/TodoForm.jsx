337a31994aa2dc6775cf2f7dbac4c92106f45c02
Daniel Cardozo C <daniel.cardozo0621@gmail.com>
2019-02-04 11:05:04 -0500

    Redux-form validations added

--- chap5/src/components/Todo/TodoForm.jsx
+++ chap5/src/components/Todo/TodoForm.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+import './TodoForm.css';
+
 class TodoForm extends Component {
     // Functional component to render an input...
     renderInput = ({ input }) => <input {...input} type="text" />;
@@ -15,22 +17,44 @@ class TodoForm extends Component {
         addTask(values);
     };
 
+    renderError(field) {
+        const { meta: { submitFailed, error } } = field;
+
+        if (submitFailed && error) {
+            return (
+                <div className="error">
+                    {error}
+                </div>
+            );
+        }
+
+        return null;
+    }
+
     render() {
         // handleSubmit is part of Redux Form to handle the onSubmit event
-        const { handleSubmit } = this.props;
+        const { handleSubmit, submitting } = this.props;
 
         return (
             <form onSubmit={handleSubmit(this.onSubmit)}>
                 {/* Field is a Redux Form Component, we need to pass the name of the input and the component
                 we are using to render it */}
-                <Field
-                    name="task"
-                    component={this.renderInput}
-                />
+                <Field name="task" component={this.renderInput} />
+                <Field name="task" component={this.renderError} />
             </form>
         )
     }
 }
 
+const validate = values => {
+    const errors = {};
+
+    if (!values.task) {
+        errors.task = 'Task cannot be empty!';
+    }
+
+    return errors;
+};
+
 // With this we named our form reducer for this specific form (todo).
-export default reduxForm({ form: 'todo' })(TodoForm);
+export default reduxForm({ validate, form: 'todo' })(TodoForm);
